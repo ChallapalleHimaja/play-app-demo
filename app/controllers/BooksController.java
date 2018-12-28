1072fb2a717920b0a277664632b67b63ca04b183
Mattias Levin <mattias_levin@apple.com>
2018-12-28 14:10:55 +0530

    Added JPA support Annotated Book as Entitity

--- app/controllers/BooksController.java
+++ app/controllers/BooksController.java
@@ -3,20 +3,28 @@ package controllers;
 import com.fasterxml.jackson.databind.JsonNode;
 import models.Book;
 import play.Logger;
+import play.db.jpa.JPAApi;
+import play.db.jpa.Transactional;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 
-import java.util.HashMap;
-import java.util.Map;
+import javax.inject.Inject;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 public class BooksController extends Controller {
 
     private final static Logger.ALogger LOGGER = Logger.of(BooksController.class);
 
-    int index = 0;
-    final Map<Integer, Book> books = new HashMap<>();
+    final JPAApi jpaApi;
 
+    @Inject
+    public BooksController(JPAApi jpaApi) {
+        this.jpaApi = jpaApi;
+    }
+
+    @Transactional
     public Result createBook() {
 
         final JsonNode json = request().body().asJson();
@@ -26,26 +34,25 @@ public class BooksController extends Controller {
         LOGGER.debug("Book title = " + book.getTitle());
         LOGGER.error("This is an error");
 
-
         if (null == book.getTitle()) {
             return badRequest("Title must be provided");
         }
 
-        book.setId(index++);
-        books.put(book.getId(), book);
+        jpaApi.em().persist(book);
 
         final JsonNode result = Json.toJson(book);
 
         return ok(result);
     }
 
+    @Transactional
     public Result getBookById(Integer id) {
 
         if (null == id) {
             return badRequest("Id must be provided");
         }
 
-        final Book book = books.get(id);
+        final Book book = jpaApi.em().find(Book.class, id);
         if (null == book) {
             return notFound();
         }
@@ -55,13 +62,14 @@ public class BooksController extends Controller {
         return ok(result);
     }
 
+    @Transactional
     public Result updateBookById(Integer id) {
 
         if (null == id) {
             return badRequest("Id must be provided");
         }
 
-        final Book existingBook = books.get(id);
+        final Book existingBook = jpaApi.em().find(Book.class, id);
         if (null == existingBook) {
             return notFound();
         }
@@ -70,32 +78,48 @@ public class BooksController extends Controller {
         final Book newBook = Json.fromJson(json, Book.class);
 
         existingBook.setTitle(newBook.getTitle());
-        books.put(existingBook.getId(), existingBook);
+        jpaApi.em().persist(existingBook);
 
         final JsonNode result = Json.toJson(existingBook);
         return ok(result);
     }
 
+    @Transactional
     public Result deleteBookById(Integer id) {
 
         if (null == id) {
             return badRequest("Id must be provided");
         }
 
-        final Book book = books.remove(id);
+        final Book book = jpaApi.em().find(Book.class, id);
         if (null == book) {
             return notFound();
         }
+
+        jpaApi.em().remove(book);
 
         final JsonNode result = Json.toJson(book);
         return ok(result);
     }
 
+    @Transactional
     public Result getAllBooks() {
 
-        final JsonNode result = Json.toJson(books.values());
+        TypedQuery<Book> query = jpaApi.em().createQuery("SELECT b FROM Book b", Book.class);
+        List<Book> books = query.getResultList();
+
+        final JsonNode result = Json.toJson(books);
 
         return ok(result);
     }
 
 }
+
+
+
+
+
+
+
+
+
--- app/models/Book.java
+++ app/models/Book.java
@@ -2,11 +2,17 @@ package models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.persistence.*;
+
+@Entity
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @JsonProperty("id")
     private Integer id;
 
+    @Basic
     @JsonProperty("title")
     private String title;
 
